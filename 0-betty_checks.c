bbde3a482ab140e1213721392727353fecdf8bb1
Hatim Bouzerioh <hatim.bouzerioh@gmail.com>
2023-08-14 15:53:16 -0700

    Write a beautiful code that passes the Betty checks

---
+++ 0-betty_checks.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+ * main - Pass the betty checks
+ * @argc: The number of items in av
+ * @argv: A NULL terminated array of strings
+ * @envp: A NULL terminated array of strings
+ *
+ * Return: Always 0 (Success).
+ */
+int main(int __attribute((unused)) argc, char **argv, char **envp)
+{
+
+	argv[0] = "/usr/local/bin/betty";
+
+	if (execve(argv[0], argv, envp) == -1)
+	{
+		exit(EXIT_FAILURE);
+	}
+
+	exit(EXIT_SUCCESS);
+}
